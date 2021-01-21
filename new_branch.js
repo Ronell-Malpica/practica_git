72315fda45539900ceef8b807345a6a3a385fbc1
Ronell Malpica <ronelll3malpica@gmail.com>
2021-01-21 00:06:18 -0500

    this is the second commit for new-branch

--- new_branch.js
+++ new_branch.js
@@ -2,3 +2,6 @@ console.log("this is for then new branch");
 function greeting() {
   return console.log("this  is new_branch  Hi world");
 }
+function secondFunction() {
+  return console.log("this is the second function");
+}
